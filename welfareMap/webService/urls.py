061b9213a2aaccd7906240c353a4bccb46becf01
frank <frank@frank>
2015-08-23 20:42:08 +0800

    add web service get user register

--- welfareMap/webService/urls.py
+++ welfareMap/webService/urls.py
@@ -3,5 +3,7 @@ from webService import views
 
 
 urlpatterns = [
+    url(r'^register/$', views.register, name='register'),
+    url(r'^getUser/$', views.getUser, name='getUser'),
     url(r'^$', views.webService, name='webService'),
 ]
--- welfareMap/webService/views.py
+++ welfareMap/webService/views.py
@@ -1,7 +1,36 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
 from django.http.response import JsonResponse
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 
 # Create your views here.
+@csrf_exempt
 def webService(request):
     return JsonResponse({'a':'b'})
+
+
+@csrf_exempt
+def register(request):
+    if request.method=='POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        name = request.POST.get('name')
+        user = User()
+        user.username = username
+        user.first_name = name
+        user.set_password(password)
+        user.save()        
+        return
+    
+@csrf_exempt
+def getUser(request):
+    if request.method=='post':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        userDic = {'username':user.username, 'password':password,
+                   'name':user.first_name}
+    return  JsonResponse(userDic)
+    
